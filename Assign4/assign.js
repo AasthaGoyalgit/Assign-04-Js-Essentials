c38ef44f3f1782c6a2cd8a29b56dcc65050e909a
AasthaGoyalgit <66162540+AasthaGoyalgit@users.noreply.github.com>
2021-01-27 18:30:28 +0530

    Add files via upload

---
+++ Assign4/assign.js
@@ -0,0 +1,10 @@
+  
+  
+  
+  function add(){
+   var a,b,c; 
+   a=Number(document.getElementById("first").value);
+   b=Number(document.getElementById("second").value);
+   c=a+b;
+  document.getElementById("ans").value=c;
+  }
